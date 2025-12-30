3b711256d9a988a4f949325b78183c581081c6b1
nmeier01 <nmeier01@student.ubc.ca>
2025-12-29 16:12:07 -0800

    Added ggplot summary stats and responded to comments

--- analyses/germ_plotting.R
+++ analyses/germ_plotting.R
@@ -13,13 +13,25 @@ library(reshape)
 
 # set wd
 # Update to include yours with ifelse
-setwd("/Users/lizzie/Documents/git/projects/grephon/soilinoculation/analyses/")
+
+user <- Sys.info()[["user"]]
+
+setwd(
+  ifelse(
+    user == "lizzie",
+    "/Users/lizzie/Documents/git/projects/grephon/soilinoculation/analyses/",
+    ifelse(
+      user == "nolanmeier",
+      "/Users/nolanmeier/git/morasoilinoculation/analyses"
+    )
+  )
+)
 
 # get the data!
 # should be INPUT (not inputs)
 aliveraw <- read.csv('inputs/alive_germinants_12_2.csv')
 alive <- aliveraw
-alive[is.na(alive)] <- 0 #  NA -> 0, is this okay?
+alive[is.na(alive)] <- 0 #  This works, all empty cells mean no germinants (of any kind) observed
 
 dead <- read.csv('inputs/dead_germinants_12_2.csv')
 
@@ -40,3 +52,32 @@ ggplot(alivelong, aes(x=as.numeric(doy), y=germinants, color=inoculated)) +
 	# it would be better if we summarized the data by inoluction level using ggplot stats etc.
 	# would be great to work on that next if someone has time!
 	# and then we could do it for dead also. 
+
+####### Working off the above to sort by inoculation ##########
+####### Dec. 29, 2025, by Nolan ########
+head(alivelong)
+
+# summarize stats by grouping by inoculation level
+# plot as lines
+ggplot(alivelong,
+       aes(x = as.numeric(doy),
+           y = germinants,
+           color = inoculated,
+           group = inoculated)) +
+  
+  stat_summary(fun = mean, geom = "line") + 
+  
+  stat_summary(
+    fun.data = mean_se,
+    geom = "errorbar",
+    width = 1
+  ) + 
+  facet_grid(seed_species ~ soil_species)
+  
+  
+
+
+alive_10 <- alivelong[alivelong$inoculated == 10,]
+alive_sterile <- alivelong[alivelong$inoculated == 0,]
+alive_25 <- alivelong[alivelong$inoculated == 25,]
+
